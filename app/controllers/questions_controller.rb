3313d2046da9ae7907872f068ba7268572cb2fdd
Chammz <topher.hammond@gmail.com>
2017-09-03 15:14:36 -0700

    added questions controller to view,update, create & delete questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -29,13 +29,26 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    @question.update_attributes(params.require(:question).permit(:title, :body, :resolved))
+    @question = Question.find(params[:id]) 
 
-    if @question.save
+    if @question.update_attributes(params.require(:question).permit(:title, :body, :resolved))
       flash[:notice] = "Question was updated."
       redirect_to @question
     else
       flash[:error] = "There was an error saving the question. Please try again."
       render :edit
     end
+  end  
+
+  def destroy
+    @question = Question.find(params[:id])  
+
+    if @question.destroy
+      flash[:notice] = "\"#{@question.title}\" was deleted successfully."
+      redirect_to questions_path
+    else  
+      flash[:error] = "There was an error deleting the question."
+      render :show
+    end  
+  end  
 end
